6efa1ced9f214c87cc1927fa856fb0d1bef2eb48
davidpknight <daveknight28@gmail.com>
2016-04-09 18:03:29 +1000

    Added publish() to fabfile.py for deploying to Github.io

--- fabfile.py
+++ fabfile.py
@@ -92,3 +92,10 @@ def gh_pages():
     rebuild()
     local("ghp-import -b {github_pages_branch} {deploy_path}".format(**env))
     local("git push origin {github_pages_branch}".format(**env))
+
+def publish():
+		local('pelican content -0 output -s pelicanconf.py')
+		local('ghp-import output')
+		local('git push https://github.com/davidpknight/davidknight.github.io.git gh-pages:master')
+		
+		
